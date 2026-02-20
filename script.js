c17f0f544ef1a750c6225f2569a81267b93cbddc
Alisson <104654059+Alissonlegall@users.noreply.github.com>
2026-02-20 08:45:45 -0300

    1

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('btnInteracao').addEventListener('click', function() {
+    alert("Dica: Comece aprendendo lógica de programação e engines como Unity ou Unreal Engine! O mercado global está em constante expansão.");
+});
